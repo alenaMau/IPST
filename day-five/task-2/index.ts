b070aa82119ad837dda4ffb9de1b5263e7216c01
AlenaMau <alenamau3486@gmail.com>
2025-01-16 22:59:33 +0700

    Исправила 2 задачу в day-five

--- day-five/task-2/index.ts
+++ day-five/task-2/index.ts
@@ -1,17 +1,17 @@
-function splittingBillAmount(sum:number):{ [key: number]: number } {
+function splittingBillAmount(sum: number): { [key: number]: number } {
     const availableBanknotes = [100, 50, 10, 5, 2, 1]
 
     const conclusion: { [key: number]: number } = {}
 
-    for(let bill of availableBanknotes) {
-        if(sum > bill) {
-            const count = Math.floor(sum / bill);
-            conclusion[bill] = count;
-            sum -= count * bill;
+    for (let bill of availableBanknotes) {
+        if (sum > bill) {
+            conclusion[bill] = Math.floor(sum / bill);
+            sum %= bill
         } else {
             conclusion[bill] = 0
         }
-    } return conclusion
+    }
+    return conclusion
 }
 
 const answer: number = 1050
